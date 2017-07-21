42796b7ca6093d61f9f6b7d0bdd4cae3dcafbce9
Matthew Hayes <mhayes@uship.com>
2017-07-21 12:05:39 -0500

    loading the cube from the scene file, but then re-constructing on load with an actual SCNBox instance for geometry

--- ARKitExample/Virtual Objects/MeasurementCube.swift
+++ ARKitExample/Virtual Objects/MeasurementCube.swift
@@ -32,6 +32,32 @@ class MeasurementCube: VirtualObject {
 //        self.position = SCNVector3(0, oneFoot/2, 0)
     }
     
+    override func loadModel() {
+        super.loadModel()
+        
+        guard let wrapperNode = childNodes.first else { return }
+        
+        wrapperNode.childNodes.forEach { (node) in
+            if let fakeBoxGeometry = node.geometry
+            {
+                let boxGeometry = SCNBox(width: 1, height:1, length:1, chamferRadius:0.0)
+                boxGeometry.materials = fakeBoxGeometry.materials
+                let boxNode = SCNNode(geometry: boxGeometry)
+                boxNode.scale = node.scale
+                boxNode.rotation = node.rotation
+                boxNode.position = node.position //SCNVector3(node.position.x, Float(oneFoot/2.0), node.position.y)
+                boxNode.transform = node.transform
+                boxNode.eulerAngles = node.eulerAngles
+                boxNode.orientation = node.orientation
+                boxNode.pivot = node.pivot
+                
+                node.removeFromParentNode()
+                wrapperNode.addChildNode(boxNode)
+            }
+        }
+        
+    }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
